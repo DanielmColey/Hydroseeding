e1d257f186b14adcda73b9594f65771638959e58
Tyson Bloxham <tysonsbloxham@gmail.com>
2017-07-26 18:50:04 -0600

    Implemented Devise and setup a test user in the seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(email: "test@test.com", 
+						password: "asdfasdf", 
+						password_confirmation: "asdfasdf", 
+						first_name: "Han", 
+						last_name: "Solo")
+
+puts "1 user created"
