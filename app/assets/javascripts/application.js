fceb849524c42f95320b3132057ee6aa6493d616
Tyson Bloxham <tysonsbloxham@gmail.com>
2017-07-29 16:46:31 -0600

    Implemented an image preview for the uploader as well as CRUD functionality

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,23 @@
 //= require bootstrap-sprockets
 //= require turbolinks
 //= require_tree .
+
+
+function readURL(input) {
+  if (input.files && input.files[0]) {
+    var reader = new FileReader();
+
+   reader.onload = function (e) {
+      $('#img_prev')
+        .attr('src', e.target.result)
+        .width(150)
+        .height(200);
+    };
+
+   reader.readAsDataURL(input.files[0]);
+  }
+}
+
+$("#imgInp").change(function(){
+	readURL(this);
+});
--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,5 +1,5 @@
 class PicturesController < ApplicationController
-	before_action :set_picture , only: [:edit]
+	before_action :set_picture , only: [:edit, :update, :destroy]
 
 	def index
   	@pictures = Picture.all
@@ -12,12 +12,30 @@ class PicturesController < ApplicationController
   def edit  	
   end
 
+  def update
+    respond_to do |format|
+      if @picture.update(picture_params)
+        format.html { redirect_to root_path, notice: 'Picture was successfully updated' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
+  def destroy
+    @picture.destroy
+
+    respond_to do |format|
+      format.html { redirect_to root_path, notice: 'Picture was successfully deleted.' }
+    end
+  end
+
  	def create
     @picture = Picture.new(picture_params)
 
    respond_to do |format|
       if @picture.save
-        format.html { redirect_to pictures_path, notice: 'You added the picture' }
+        format.html { redirect_to pictures_path, notice: 'Picture was successfully added' }
       else
         format.html { render :new }
       end
