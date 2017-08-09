e45f67f27c5cb13474e8693e492dc4f061eae8fa
Stephani Gonzalez <spgonza@live.com>
2017-08-09 13:16:09 -0600

    changed fonts and finished home page

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   get 'contact', to: 'pages#contact'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'pictures#index'
+  root to: 'pages#index'
 
   get "/contact", to: "pages#contact"
   
